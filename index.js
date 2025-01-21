c9a2e43dc390215d64871bc53177a2cb6d7fb69c
Tsion <tsion714@gmail.com>
2025-01-20 19:11:30 -0500

    update

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
-fetch('http://www.omdbapi.com/?apikey=6d084915&s=fast')
-.then(Response => JSON())
-.then(data => {
-    console.log(data);
-})
-.catch(error => console.error('Error fetching data:', error));
+let movies = [];
 
-console.log(fetch(`http://www.omdbapi.com/?apikey=6d084915&s=fast`));
-//function loadMovies(){
+async function loadMovies(filter = '') {
+    const moviesWrapper = document.querySelector('.movies');
+    moviesWrapper.classList += 'movies__loading'
+    if(!movies){
+        movies = await loadMovies();
+    }
+
+    moviesWrapper.classList.add('movies__loading'); 
+
+    try {
+        const response = await fetch(`http://www.omdbapi.com/?apikey=6d084915&s=fast`);
+        const data = await response.json();
+        
+        if (data.Response === 'True') {
+            movies = data.Search;
+
+            const moviesHtml = movies
+                .map((movie) => {
+                    return `
+                        <div class="movie">
+                            <figure class="movie__img--wrapper">
+                                <img class="movie__img" src="${movie.Poster}" alt="${movie.Title}">
+                            </figure>
+                            <div class="movie__title">${movie.Title}</div>
+                            <div class="movie__year">${movie.Year}</div>
+                        </div>
+                    `;
+                })
+                .join("");
 
 
-//const moviesWrapper = `http://www.omdbapi.com/?apikey=6d084915&s=fast`
+            moviesWrapper.innerHTML = moviesHtml; 
+        } else {
+            moviesWrapper.innerHTML = `<p>No movies found for "fast". Please try a different search term.</p>`;
+        }
+    } catch (error) {
+        console.error("Error fetching movies:", error);
+        moviesWrapper.innerHTML = `<p>There was an error fetching the movie data. Please try again later.</p>`;
+    }
+}
 
-//const moviesList = getMoveis
-//async function 
+function filterMovies(event) {
+    const filter = event.target.value;
+    loadMovies(filter); 
+}
 
-// FAKE DATA
-
+const searchInput = document.querySelector('#movie-search');
+searchInput.addEventListener('input', filterMovies); 
