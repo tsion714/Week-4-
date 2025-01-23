0707825902511f2a213e51f7a3514f96d947f555
Tsion <tsion714@gmail.com>
2025-01-22 19:27:34 -0500

    update

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ let movies = [];
 
 async function loadMovies(filter = '') {
     const moviesWrapper = document.querySelector('.movies');
-    moviesWrapper.classList += 'movies__loading'
-    if(!movies){
-        movies = await loadMovies();
-    }
+    const loadingSpinner = document.querySelector('.movies__loading');
+    
 
-    moviesWrapper.classList.add('movies__loading'); 
-
+    loadingSpinner.style.display = 'flex';
+    moviesWrapper.innerHTML = '';
     try {
         const response = await fetch(`http://www.omdbapi.com/?apikey=6d084915&s=fast`);
         const data = await response.json();
@@ -29,21 +27,23 @@ async function loadMovies(filter = '') {
                     `;
                 })
                 .join("");
-
-
-            moviesWrapper.innerHTML = moviesHtml; 
+            moviesWrapper.innerHTML = moviesHtml;
         } else {
             moviesWrapper.innerHTML = `<p>No movies found for "fast". Please try a different search term.</p>`;
         }
     } catch (error) {
         console.error("Error fetching movies:", error);
         moviesWrapper.innerHTML = `<p>There was an error fetching the movie data. Please try again later.</p>`;
+    } finally {
+       
+        loadingSpinner.style.display = 'none';
     }
 }
 
 function filterMovies(event) {
     const filter = event.target.value;
     loadMovies(filter); 
+
 }
 
 const searchInput = document.querySelector('#movie-search');
